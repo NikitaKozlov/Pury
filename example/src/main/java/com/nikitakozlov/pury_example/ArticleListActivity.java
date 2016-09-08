4493fa4de1fe2d136140ca53a603fa911251cc7e
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2016-09-08 22:53:36 +0200

    Pagination now works

--- example/src/main/java/com/nikitakozlov/pury_example/ArticleListActivity.java
+++ example/src/main/java/com/nikitakozlov/pury_example/ArticleListActivity.java
@@ -11,19 +11,39 @@ import android.view.View;
 
 public class ArticleListActivity extends AppCompatActivity {
 
+    private static final int VISIBLE_THRESHOLD = 8;
     private final static int ARTICLE_PADDING = 4;
     private final static int SPAN_COUNT = 2;
 
     private final ArticleDataSource mArticleDataSource = new ArticleDataSource();
 
+    private final RecyclerView.OnScrollListener scrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(final RecyclerView recyclerView, final int dx, final int dy) {
+            if (mIsLoading) {
+                return;
+            }
+            LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+            int visibleItemCount = layoutManager.getChildCount();
+            int totalItemCount = layoutManager.getItemCount();
+            int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount - VISIBLE_THRESHOLD) {
+                loadNextPage();
+            }
+        }
+    };
+
     private ArticleAdapter mArticleAdapter;
-    private int mPage = 1;
+    private int mPage = 0;
+    private boolean mIsLoading;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
         initRecyclerView();
+        loadNextPage();
     }
 
     private void initRecyclerView() {
@@ -42,8 +62,14 @@ public class ArticleListActivity extends AppCompatActivity {
         });
         mArticleAdapter = new ArticleAdapter();
         recyclerView.setAdapter(mArticleAdapter);
-        mArticleAdapter.addItems(mArticleDataSource.getNextPage(mPage));
+        recyclerView.setOnScrollListener(scrollListener);
     }
 
+    private void loadNextPage() {
+        mIsLoading = true;
+        mArticleAdapter.addItems(mArticleDataSource.getNextPage(mPage + 1));
+        mPage++;
+        mIsLoading = false;
+    }
 
 }
